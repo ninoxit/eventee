5c18a7c9b33287f52a9f22f2d4d12d5621b84b49
ninoxander <za220111414@zapopan.tecmm.edu.mx>
2024-04-29 13:44:20 -0600

    Añadiendo un checkpoint: añadí firebase al proyecto para ya comenzar a manejar la autenticación de los usuarios. Lo pongo como checkpoint por si de aquí en adelante ya no funciona algo regresemos a este commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { RouterProvider, createRouter } from '@tanstack/react-router';
 import { routeTree } from './routeTree.gen';
+
+
 import './styles/App.less'
 
 const router = createRouter({ routeTree });
